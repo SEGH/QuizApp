913a5bf9d7d26233313a7f8790ae55db8a6f1dda
Susan Holland <segh@fastmail.com>
2020-06-08 18:13:52 -0400

    Added pseudocode

---
+++ script.js
@@ -0,0 +1,12 @@
+// Create array of question objects
+
+// Event listener for start button
+    // Starts Quiz Function
+
+// Quiz function
+    // Timer counting down
+    // Loop through array of question objects
+        // Set button h1 as current object choices
+        // Event listener for choice button clicks
+        // If incorrect answer clicked, subtract from countdown
+    
