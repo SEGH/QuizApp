8f274c60d9cc7bfa54acef7ec856d0531ba062ca
Susan Holland <segh@fastmail.com>
2020-06-09 17:50:57 -0400

    Added question array and showQuestion function

--- script.js
+++ script.js
@@ -2,8 +2,16 @@
 var startSection = document.getElementById("startPage");
 var startButton = startSection.querySelector("button");
 var quiz = document.getElementById("quizPage");
+var question = document.getElementById("question");
+var questionIndex = 0;
+var quizButtons = quiz.querySelectorAll("button");
 
 // Create array of question objects
+var questionArray = [
+    { question: "What is the answer to this?", answers: ["a", "b", "c", "d"], correct: "a" },
+    { question: "another for you?", answers: ["my answer", "your answer", "answer", "hmm"], correct: "hmm" },
+    { question: "what about this?", answers: ["this", "that", "the other one", "final"], correct: "final" }
+];
 
 //Timer function
 
@@ -13,6 +21,7 @@ startButton.addEventListener("click", function() {
     quiz.style.display = "flex";
     startSection.style.display = "none";
     // Call quiz function
+    showQuestion();
     // Start timer
 });
 
@@ -22,7 +31,31 @@ startButton.addEventListener("click", function() {
     // Call Quiz function
 
 // Quiz function
+function showQuestion() {
     // Display current question object question
+    question.textContent = questionArray[questionIndex].question;
     // Display current question object answers as button text content
+    for (var i = 0; i < quizButtons.length; i++) {
+        quizButtons[i].textContent = questionArray[questionIndex].answers[i];
+    }
+}
+    
+    
     // If timer reaches zero or end of question array is reached
         // Display score screen
+
+// // Quiz function
+// function runQuiz() {
+//     // Timer counting down
+//     // Loop through array of question objects
+//     question.textContent = questionArray[questionIndex].question;
+//         for (var j = 0; j < questionArray[questionIndex].answers.length; j++) {
+//             // Set buttons as current object choices
+//             quizButtons[j].textContent = questionArray[questionIndex].answers[j];
+//         }
+
+
+//         // Event listener for choice button clicks
+//         clickButton();
+
+// }
